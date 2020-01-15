ed0a6821513fa7d30e05d4f5da49757531d04d4b
Pascu Norbert <norbert.pascu@softvision.ro>
2020-01-15 17:24:37 +0200

    Added context to state.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
-const IndexScreen = () =>{
-return(
-    <View>
-        <Text>Index Screen</Text>
-    </View>
-)
+const IndexScreen = () => {
+    const { data, addBlogPost } = useContext(BlogContext);
+
+    return (
+        <View>
+            <Text>Index Screen</Text>
+            <Button
+                title="Add Blog Post"
+                onPress={addBlogPost}
+            />
+            <FlatList
+                data={data}
+                keyExtractor={blogPost => blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <Text>{item.title}</Text>
+                    )
+                }}
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-
-})
+});
 
 export default IndexScreen;
